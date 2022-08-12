0d7e35e4309211d0c8f6a9923c38e4b3dfd959ae
urlastway <andriy.semenchenko13@gmail.com>
2022-08-12 16:36:34 +0200

    first commit

---
+++ abuot.php
@@ -0,0 +1,15 @@
+<?php
+    require_once("variables/VariablesAbout.php");
+    require_once("style/style.php");   
+    require_once("blocks/header.php")
+?>
+
+<body>
+    <div class="container">
+        <h1>About Content</h1>
+    </div>
+</body>
+
+<?php 
+    require_once("blocks/footer.php")
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    require_once("variables/VariablesHome.php");
+    require_once("style/style.php"); 
+    require_once("blocks/header.php");
+?>
+
+<body>
+    <div class="container">
+        <h1>Home Content</h1>
+    </div>
+</body>
+
+
+<?php 
+    require_once("blocks/footer.php");
+?>
