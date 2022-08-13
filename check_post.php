12b831cf036a0dc7e0b344497dd65e8ca75b6d8e
urlastway <andriy.semenchenko13@gmail.com>
2022-08-13 15:24:24 +0200

    add check_post

--- check_post.php
+++ check_post.php
@@ -1,2 +1,19 @@
 <?php
-print_r($_POST);
+
+$name = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+if(trim($name) == ""){
+    echo "Non è stato scritto il nome utente";
+}else if(strlen(trim($name)) <= 1){
+    echo "Il nome utente non è coretto";
+}else if(trim($email) == "" || trim($password) == "" || trim($_POST['massege']) == "" ){
+    echo "Non sono stati inseriti tutti dati";
+}else{
+    echo "<h1>Tutti i Dati</h1>
+          <p>Nome: $name</p>
+          <p>Email: $email</p>
+          <p>Password: $password</p>
+          <p>Messaggio: $_POST[massege]<p>";
+}
