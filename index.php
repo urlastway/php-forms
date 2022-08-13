177c2c31805b346b83b0f420dad1d9dc7419b394
urlastway <andriy.semenchenko13@gmail.com>
2022-08-13 14:26:59 +0200

    aggiungo form

--- index.php
+++ index.php
@@ -5,8 +5,15 @@
 ?>
 
 <body>
-    <div class="container">
+    <div class="container mt-4">
         <h1>Home Content</h1>
+        <form action="check_post.php" method="POST">
+            <input type="text" name="username" placeholder="Nome del utente" class="form-control mt-2">
+            <input type="email" name="email" placeholder="Email" class="form-control mt-2">
+            <input type="password" name="password" placeholder="Password" class="form-control mt-2">
+            <textarea name="massege" placeholder="Massege" class="form-control mt-2"></textarea>
+            <input type="submit" value="Send" class="btn btn-success mt-2">
+        </form>
     </div>
 </body>
 
